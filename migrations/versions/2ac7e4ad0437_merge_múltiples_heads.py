a95be7b803dc37743d779acf84aaf38a3bc7c164
Angel Burgos <burgosangel2001@gmail.com>
2025-04-29 23:30:02 -0600

    🔀 Merge múltiples heads

---
+++ migrations/versions/2ac7e4ad0437_merge_múltiples_heads.py
@@ -0,0 +1,24 @@
+"""Merge múltiples heads
+
+Revision ID: 2ac7e4ad0437
+Revises: 9c9791198b48
+Create Date: 2025-04-29 23:29:43.058717
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2ac7e4ad0437'
+down_revision = '9c9791198b48'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
